155133536e51e8f16adef6ad4725eed0c9ff30f7
arkan.alexei <arkan.alexei@ui.ac.id>
2023-12-08 00:40:46 +0700

    fix: nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  pagesDir: "src/pages",
   images: {
     domains: ["commons.wikimedia.org"],
     unoptimized: true,
